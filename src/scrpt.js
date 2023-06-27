9aacd3eab8b0920b37038890945b2304b4a84888
MariKhorbaladze <129887035+MariKhorbaladze@users.noreply.github.com>
2023-06-27 07:32:29 +0400

    FIIINAAAAL

--- src/scrpt.js
+++ src/scrpt.js
@@ -1,4 +1,352 @@
-function toggleShow () {
-    var el = document.getElementById("box");
-    el.classList.toggle("show");
-  }
+
+
+var photos = [
+  {
+    url: "https://nypost.com/wp-content/uploads/sites/2/2023/05/NYPICHPDPICT000010127200.jpg",
+    width: 300,
+    height: 250
+  },
+  
+  {
+    url: "https://images.squarespace-cdn.com/content/v1/5ede2122e582b96630a4a73e/1609347443412-BY6YPT4NB3RHTLCNZ7O7/image-asset.jpeg?format=2500w",
+    width: 300,
+    height: 250
+  },
+  {
+    url: "https://www.investopedia.com/thmb/diMRGUPbZ4-I2a4VMyeTx5-TMLs=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-1349988090-cd961ecf897c4c3ba0502c9db09c59af.jpg",
+    width: 300,
+    height: 250
+  },
+  {
+    url: "https://images.squarespace-cdn.com/content/v1/5ede2122e582b96630a4a73e/1609375996518-DZU53FYNB3FMBYB1JHG6/HP-logo+2021.jpg?format=2500w",
+    width: 300,
+    height: 250
+  },
+   {
+    url: "https://images.squarespace-cdn.com/content/v1/5ede2122e582b96630a4a73e/1609375769634-EG1WOTIN7Y4MB01N8AV1/Domino%E2%80%99s-logo-2021.jpg?format=2500w",
+    width: 300,
+    height: 250
+  },
+  {
+    url: "https://images.squarespace-cdn.com/content/v1/5ede2122e582b96630a4a73e/1609354030550-9ZOL45B4SJTEL9DLA12J/KFC-Logo-2021.jpg?format=2500w",
+    width: 300,
+    height: 250
+  },
+];
+
+
+var container = document.getElementById("photoContainer");
+
+photos.forEach(function(photo) {
+  var img = document.createElement("img");
+
+  img.src = photo.url;
+
+  img.width = photo.width;
+  img.height = photo.height;
+
+  img.classList.add("photo");
+
+  container.appendChild(img);
+});
+
+
+
+
+// // Get the link element
+ainer.appendChild(paragraph);
+//
+// });
+
+3
+// Get the link element
+var newLink = document.getElementById("newLink");
+
+// Get the section element where you want to navigate
+var newSection = document.getElementById("newSection");
+
+// Add a click event listener to the link
+newLink.addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent the default behavior of the link
+
+  // Scroll to the section
+  newSection.scrollIntoView({ behavior: "smooth" });
+});
+
+
+// 50vacancies
+page = 3
+pagesize = 10
+start = pagesize*page-pagesize
+end = pagesize*page
+
+{
+  "job-count"; 50,
+  "jobs" [
+    {
+      "id": 1713457,
+      "url": "https://remotive.com/remote-jobs/hr/talent-partner-1713457",
+      "title": "Talent Partner",
+      "company_name": "Atlan",
+      "company_logo": "https://remotive.com/job/1713457/logo",
+      "category": "Human Resources",
+      "tags": [
+        "data science",
+        "go",
+        "salesforce",
+        "video",
+        "Figma",
+        "agile",
+        "healthcare",
+        "Snowflake",
+        "startup",
+        "troubleshooting",
+        "github"
+      ],
+      "job_type": "full_time",
+      "publication_date": "2023-06-26T01:39:47",
+      "candidate_required_location": "USA",
+      "salary": "1500%"
+    }
+  ]
+}
+
+  "id"; 1719032,
+  "url"; "https://remotive.com/remote-jobs/data/data-protection-manager-3483-1719032",
+  "title";"Data Protection Manager (3483)",
+  "company_name"; "GBG",
+  "company_logo"; "https://remotive.com/job/1719032/logo",
+  "category"; "Data",
+  "tags"; [
+      "chat",
+      "travel",
+      "CCPA",
+  ],
+  "job_type"; "full_time",
+  "publication_date"; "2023-06-25T23:40:00",
+  "candidate_required_location"; "USA",
+  "salary"; "2000$" 
+
+
+  
+    "id"; 1713100,
+    "url"; "https://remotive.com/remote-jobs/marketing/vp-marketing-1713100",
+    "title"; "VP, Marketing ",
+    "company_name"; "ReCharge",
+    "company_logo"; "https://remotive.com/job/1713100/logo",
+    "category"; "Marketing",
+    "tags"; [
+        "ecommerce",
+        "saas",
+        "salesforce",
+        "seo",
+        "social media",
+        "product marketing",
+        "content marketing",
+        "digital marketing",
+        "content strategy",
+        "account management",
+        "partnerships",
+        "lead generation",
+        "direct sales",
+        "performance marketing"
+    ],
+    "job_type"; "",
+    "publication_date"; "2023-06-25T23:39:43",
+    "candidate_required_location"; "Worldwide",
+    "salary"; "4000$"
+
+
+    "id"; 1717294,
+            "url"; "https://remotive.com/remote-jobs/software-dev/application-support-engineer-1717294",
+            "title"; "Application Support Engineer",
+            "company_name"; "Billtrust",
+            "company_logo"; "https://remotive.com/job/1717294/logo",
+            "category"; "Software Development",
+            "tags"; [
+                "cloud",
+                "go",
+                "java",
+                "javascript",
+                "python",
+                "sql",
+                "UI/UX",
+                "agile",
+                "automation",
+                "data analysis",
+                "business development",
+                "MySQL",
+                "scripting",
+                "Linux/Unix",
+                "computer science",
+                "InDesign",
+                "diversity",
+                "insurance"
+            ],
+            "job_type"; "full_time",
+            "publication_date"; "2023-06-25T23:39:33",
+            "candidate_required_location"; "USA",
+            "salary"; "2000#"
+
+            "id"; 1710673,
+            "url"; "https://remotive.com/remote-jobs/writing/entertainment-writer-1710673",
+            "title"; "Entertainment Writer",
+            "company_name"; "GAMURS Group",
+            "company_logo"; "https://remotive.com/job/1710673/logo",
+            "category"; "Writing",
+            "tags"; [
+                "social media",
+                "wordpress"
+            ],
+            "job_type"; "freelance",
+            "publication_date"; "2023-06-25T21:39:08",
+            "candidate_required_location"; "USA",
+            "salary"; "3000#"
+
+            "id"; 1712782,
+            "url"; "https://remotive.com/remote-jobs/product/product-manager-1712782",
+            "title"; "Product Manager",
+            "company_name"; "Blackbird.AI",
+            "company_logo"; "https://remotive.com/job/1712782/logo",
+            "category"; "Product",
+            "tags"; [
+                "business intelligence",
+                "saas",
+                "security",
+                "AI/ML",
+                "research",
+                "product management",
+                "market research",
+                "platform security"
+            ],
+            "job_type"; "full_time",
+            "publication_date"; "2023-06-25T21:38:59",
+            "candidate_required_location"; "USA",
+            "salary"; "4800$"
+
+            "id"; 1693688,
+            "url"; "https://remotive.com/remote-jobs/product/product-owner-1693688",
+            "title"; "Product Owner",
+            "company_name"; "roadie",
+            "company_logo"; "https://remotive.com/job/1693688/logo",
+            "category"; "Product",
+            "tags"; [
+                "UI/UX",
+                "agile",
+                "research",
+                "product management",
+                "product development",
+                "product owner",
+                "insurance"
+            ],
+            "job_type"; "",
+            "publication_date"; "2023-06-25T19:39:38",
+            "candidate_required_location";"USA",
+            "salary"; ""  
+
+            "id"; 1606658,
+            "url"; "https://remotive.com/remote-jobs/software-dev/senior-unity-developer-1606658",
+            "title"; "Senior Unity Developer",
+            "company_name"; "Proxify",
+            "company_logo"; "https://remotive.com/job/1606658/logo",
+            "category"; "Software Development",
+            "tags"; [
+                "C",
+                "C#",
+                "unity",
+                "unity3D",
+                "Typescript ",
+                "scripting",
+                "troubleshooting",
+                "testing"
+            ],
+            "job_type"; "full_time",
+            "publication_date"; "2023-05-28T16:01:02",
+            "candidate_required_location"; "Worldwide",
+            "salary"; "$40k - $70k"
+
+            "id"; 1697173,
+            "url"; "https://remotive.com/remote-jobs/finance-legal/staff-accountant-1697173",
+            "title"; "Staff Accountant",
+            "company_name";"Discourse",
+            "company_logo"; "https://remotive.com/job/1697173/logo",
+            "category";"Finance / Legal",
+            "tags"; [
+                "accounting",
+                "admin",
+                "excel",
+                "saas",
+                "open source",
+                "blockchain",
+                "payroll",
+                "onboarding",
+                "SOLID",
+                "startup",
+                "banking"
+            ],
+            "job_type"; "full_time",
+            "publication_date"; "2023-05-30T18:18:03",
+            "candidate_required_location"; "USA",
+            "salary"; "25k"
+
+            "id"; 1576731,
+            "url"; "https://remotive.com/remote-jobs/software-dev/migrations-specialist-1576731",
+            "title"; "Migrations Specialist",
+            "company_name"; "Discourse",
+            "company_logo"; "https://remotive.com/job/1576731/logo",
+            "category"; "Software Development",
+            "tags" [
+                "admin",
+                "CSS",
+                "postgresql",
+                "ruby/rails",
+                "sql",
+                "open source",
+                "blockchain",
+                "MySQL",
+                "debian",
+                "chat",
+                "troubleshooting",
+                "Linux/Unix",
+                "infrastructure"
+            ],
+            "job_type"; "full_time",
+            "publication_date"; "2023-05-31T00:01:05",
+            "candidate_required_location"; "Americas",
+            "salary"; "39k"
+
+            "id"; 1697847,
+            "url"; "https://remotive.com/remote-jobs/business/start-up-investment-analyst-1697847",
+            "title"; "Start Up Investment Analyst",
+            "company_name"; "fail ventures",
+            "company_logo"; "https://remotive.com/job/1697847/logo",
+            "category"; "Business",
+            "tags"; [
+                "social media",
+                "documentation",
+                "SOLID"
+            ],
+            "job_type"; "full_time",
+            "publication_date"; "2023-05-31T11:48:27",
+            "candidate_required_location"; "Worldwide",
+            "salary"; "$45k - $55k"
+
+            "id"; 1699349,
+            "url"; "https://remotive.com/remote-jobs/marketing/director-of-marketing-1699349",
+            "title"; "Director of Marketing",
+            "company_name"; "WhiteHat Engineering",
+            "company_logo"; "https://remotive.com/job/1699349/logo",
+            "category"; "Marketing",
+            "tags"; [
+                "video",
+                "research",
+                "market research",
+                "product development",
+                "customer acquisition",
+                "brand design",
+                "user acquisition"
+            ],
+            "job_type"; "full_time",
+            "publication_date"; "2023-06-02T06:51:55",
+            "candidate_required_location"; "USA",
+            "salary"; "89k"
